354b040a0f374de6b68f4aa5f8e5ad57b9a5ef70
Rahul <rahulmishra4317@gmail.com>
2025-03-15 00:44:13 +0530

    Show me the code : Props

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./sources/ApplicationLogo.jpg";
+import logo2 from "/sources/Application-logo-2.jpg";
+import meghanaBriyaniImage from "./sources/Meghana-Foods.avif";
 /*
 - Header
     - Logo
@@ -8,6 +11,11 @@ import ReactDOM from 'react-dom/client'
     - Search bar component
     - Card container : Restaurant cards component
         - Restaurant Cards
+            - Image of the restaurant 
+            - Description 
+            - Rating 
+            - Price 
+            - Time to deliver
 - Footer
     - Copyright
     - Links
@@ -16,61 +24,83 @@ import ReactDOM from 'react-dom/client'
 */
 
 const AppLayout = () => {
-    return (
-        <div className = "app">
-             <HeaderComponent/>
-             <BodyComponent/>
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <BodyComponent />
+    </div>
+  );
+};
 
 const HeaderComponent = () => {
-    return (
-        <div className = "header">
-            <div className = "logo-container">
-                <img className = "logo" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7eaTHYrwbIDLDcu6q9UGxpa6vZ8aRoMzfQ&s" /> 
-            </div>
-            <div className = "nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact US</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-            
-        </div>
-    )
-}
+  /**
+    - Header
+        - Logo
+        - Nav Items
+     */
+  return (
+    <div className="header">
+      {/* Logo */}
+      <div className="logo-container">
+        <img src={logo2} className="logo" />
+      </div>
+      {/* Navigation Items */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const BodyComponent = () => {
-    return (
-        <div className = "body">
-            <div className = "search">
-                Search bar 
-            </div>
-            <div className='restaurant-container'>
-                <RestaurantCardComponent/>
-            </div>
-        </div>
-    )
-}
+  return (
+    /*
+        - Body
+        - Search bar component
+        - Card container : Restaurant cards component
+        */
+    <div className="body">
+      {/* Search Bar */}
+      <div className="search">Search</div>
 
-const RestaurantCardComponent = () => {
-    
-    return (
-        <div className = "restaurant-card">
-            {/* 
-            - Image of the restaurant 
-            - Resturant name in heading
-            
-            */}
-            <img className="res-logo" src ="https://shorturl.at/7FjSX"/>
-            <h3 className = "res-title">Meghana Foods</h3>
-            <h4>Andhra</h4>
-        </div>
-    )
-}
+      {/* Card Container */}
+      <div className="res-card-container">
+        <RestaurantCardContainer
+          resName="Meghana Foods"
+          resCuisine="Andhra Style"
+        />
+        <RestaurantCardContainer
+          resName="KFC"
+          resCuisine="Burget , Fast Food"
+        />
+      </div>
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<AppLayout/>)
+const RestaurantCardContainer = ({resName , resCuisine}) => {
+  /*
+     - Restaurant Cards
+        - Image of the restaurant 
+        - Description 
+        - Rating 
+        - Price 
+        - Time to deliver
+     */
+  return (
+    <div className="res-card">
+      <img src={meghanaBriyaniImage} className="res-card-logo" />
+      <h4 className="res-desc">{resName}</h4>
+      <h5 className="res-cuisine">{resCuisine}</h5>
+      <h5 className="res-rating">4.4 ✬ Rating</h5>
+      <h5 className="res-timing">38 Minutes</h5>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
