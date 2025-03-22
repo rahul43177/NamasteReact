34697a90fe61f2604d9a9f0f3a6b9575f4087e72
Rahul <rahulmishra4317@gmail.com>
2025-03-22 21:00:48 +0530

    Episode 5 : Exporting and Importing the module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {  HeaderComponent } from "./components/HeaderComponent";
+import { HeaderComponent } from "./components/HeaderComponent";
 import {BodyComponent} from './components/BodyComponent'
 
 
